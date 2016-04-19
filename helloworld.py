b3fe8e1df395809de333a3123db3b2547fbf0968
Obeast87 <obeast@gmail.com>
2016-04-18 22:54:08 -0700

    Added files via upload

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+answer = raw_input ("Hello World! Are you gay? ").lower()
+
+if answer == "yes" or answer == "y":
+	print "I knew it!"
+
+elif answer == "no" or answer == "n":
+	print "You're lying."
+
+elif answer == "you're gay" or answer == "you" or answer == "u":
+	print "MY GAY TRUTH HAS BEEN DISCOVERED. FORMATTING HARD DRIVE."
+
+else:
+	print "Fine, be that way. I know the truth about you, gay boy."
---
+++ readme.txt
@@ -0,0 +1,8 @@
+This is the most advanced Hello World in existance. Can you solve it's deep mystery?
+
+________________________
+Release History/Changelog
+________________________
+
+Version 1.0 (4/18/2016)
+-Initial Release
